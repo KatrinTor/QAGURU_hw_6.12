5d1285eaf4df03a252c979dac9d6de3f73cf9217
Торсунова Екатерина Павловна <katrin.tep@list.ru>
2023-10-08 16:50:05 +0300

    Внесены изменения в conftest.py

--- conftest.py
+++ conftest.py
@@ -6,9 +6,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import browser
 from dotenv import load_dotenv
-from .utils import attach as attach
 
-import tests
+import tests as tests
+from .utils import attach as attach
 
 DEFAULT_BROWSER_VERSION = "100.0"
 
